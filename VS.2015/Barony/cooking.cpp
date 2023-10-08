aa7468e495d029789422847240b400972ce6b7bb
Lynx <lynx223300@gmail.com>
2023-10-08 07:46:34 -0700

    Cooking - Added to CMakeLists.txt
    Also began to try and do my own code; Pretty bad at it though

---
+++ VS.2015/Barony/cooking.cpp
@@ -0,0 +1,7 @@
+#include "cooking.hpp"
+#include "items.hpp"
+
+
+
+
+	
---
+++ VS.2015/Barony/cooking.hpp
@@ -0,0 +1,21 @@
+/*-------------------------------------------------------------------------------
+
+	BARONY
+	File: cooking.h
+	Desc: contains names and definitions for cooking
+
+-------------------------------------------------------------------------------*/
+
+
+#pragma once
+
+#include "main.hpp"
+
+int isFoodRaw()
+{
+	int foodRaw = 0;
+}
+int isFoodCooked()
+{
+	int foodCooked = 0;
+}
